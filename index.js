c2d8e519f1a0d7ff18f5217ea75a3aa146cd8dd4
Maxwell DeVos <maxdevos49@gmail.com>
2018-04-07 14:51:45 -0500

    Updated server to express and fixed some links with the resume

--- index.js
+++ index.js
@@ -1,97 +1,33 @@
-/*global require:true, console:true */
-"use strict";
 
-//require http module componets
-var http = require('http');
-//require url module components
-var url = require('url');
-//require file system components
-var fs = require('fs');
-//require chalk module
-var chalk = require('chalk');
+//require server components
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const ip = require('ip');
+const port = process.env.PORT || 8080;
 
-var serverVersion = 0.1;
+//let the server listen on port 80
+server.listen(port);
 
-const error = chalk.keyword('red');
-//const warning = chalk.keyword('yellow');
-const info = chalk.keyword('white');
+//tell console the server is running
+console.log(`ThatOneSpot is Running! IP is ${ip.address()}:${port}`);
 
+//tell where to find page depended info. css/javascript/images/audio/and other stuff
+app.use(express.static('public'));
 
-fs.readFile('consoleLog.txt', function(err, data) {
-  if (err){
-    fs.appendFile('consoleLog.txt', getTime() + "Server Log Created!"+ "\n" + getTime() + "SERVER STARTED. Version: " + serverVersion + "\n", function(err) {
-      if (err) throw err;
-      console.log(getTime() + info("Server Log Created!" + "\n" + getTime() + "SERVER STARTED. Version: " +serverVersion + "\n"));
-    });
-  }else{
-    fs.appendFile('consoleLog.txt', getTime() + "SERVER STARTED. Version: " + serverVersion+ "\n", function(err) {
-      if (err) throw err;
-      console.log(getTime() + info("Server Started Version: " + serverVersion));
-    });
+//do this on a connection from the client
+app.get('/', (req, res) => {
 
-  }
+  //respond with this for the client
+  res.sendFile(__dirname + '/index.html');
+  console.log("Connection!");
+
 });
 
+app.get('/index.html', (req, res) => {
 
-//create a http server
-http.createServer(function (req, res) {
+  //respond with this for the client
+  res.sendFile(__dirname + '/index.html');
+  console.log("Connection!");
 
-
-  var q = url.parse(req.url, true);
-  //print q
-  fs.appendFile('consoleLog.txt', getTime() + "New connection to " + q.pathname + "\n", function(err){
-    if (err) throw err;
-      console.log(getTime() + info("New connection to " + q.pathname));
-  })
-  
-
-  var filename = "." + q.pathname;
-
-  if (filename == "./"){//do this if its the homepage
-
-
-    fs.readFile("./index.html", function(err, data) {
-
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      //end response here 
-      return res.end();
-    });
-
-  }else{
-    //do this if its a normal page or 404
-
-    fs.readFile(filename, function(err, data) {
-
-      if (err) {
-
-        console.log(getTime() + error("404 " + err));
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        //end response here with 404
-        return res.end("404 Not Found");
-
-      }  
-
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      //end response here 
-      return res.end();
-    });
-  }
-
-}).listen(8080);
-
-
-function getTime(){
-
-  var date = new Date();
-
-  var hours = date.getHours();
-
-  var minutes = date.getMinutes();
-
-  var seconds = date.getSeconds();
-
-  return hours + ":" + minutes + ":" + seconds + "-->";
-
-}
+});
