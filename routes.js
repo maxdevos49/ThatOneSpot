68eef0109c2862f28dd66463fbd1bdc901fc301c
Maxwell DeVos <maxdevos49@gmail.com>
2018-04-18 11:52:05 -0500

    restructured some more files. added pacman to the new rending system and added a 404 page

--- routes.js
+++ routes.js
@@ -6,6 +6,27 @@ module.exports = function(app) {
     require('./views/gallery/routes.js')(app);
     require('./views/resume/routes.js')(app);
     require('./views/projects/routes.js')(app);
+    require('./views/pacman/routes.js')(app);
+
+    //404 error because nothing else was found
+    app.use(function(req, res, next){
+        res.status(404);
+      
+        // respond with html page
+        if (req.accepts('html')) {
+          res.render('./404/404', { url: req.url });
+          return;
+        }
+      
+        // respond with json
+        if (req.accepts('json')) {
+          res.send({ error: 'Not found' });
+          return;
+        }
+      
+        // default to plain-text. send()
+        res.type('txt').send('Not found');
+      });
 
 
 
